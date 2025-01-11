56cd5aeb570e886165be202a2d5d17ff6a9f8e64
Gonzalo Comeron <gonza_agl@hotmail.com>
2025-01-10 18:36:06 -0600

    Sql Practice

--- SQLLevelPractice.sql
+++ SQLLevelPractice.sql
@@ -251,3 +251,15 @@ FROM Employees
 
 
 -- Ver "SQLLevelPractice.docx" 'SQL28'
+
+SELECT
+    CASE 
+        WHEN (A+B) <= C OR (A+C) <= B OR (B+C) <= A THEN "Not A Triangle"
+        WHEN (A = B) AND (A = C) AND (B = C) THEN "Equilateral"
+        WHEN (A = B) OR (A = C) OR (B = C) THEN "Isosceles"
+        WHEN (A <> B) AND (A <> C) AND (B <> C) THEN "Scalene"
+    END AS TypeofTriangle
+FROM Triangles
+
+
+-- Ver "SQLLevelPractice.docx" 'SQL29'
